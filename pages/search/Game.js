adca4ad2bf4bd83af96d34e90c3e7411d4d43ee5
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-13 11:01:03 -0500

    All game images work and display a placeholder if no image exists

--- pages/search/Game.js
+++ pages/search/Game.js
@@ -7,7 +7,11 @@ const Game = ({ game }) => {
     <div className="m-2">
       <Link href={`/games/${game.id}`} passHref>
         <Image
-          src="https://images.igdb.com/igdb/image/upload/t_cover_big/nocover.png"
+          src={
+            game.hasOwnProperty('cover')
+              ? `https://images.igdb.com/igdb/image/upload/t_1080p/${game.cover.image_id}.jpg`
+              : `https://images.igdb.com/igdb/image/upload/t_cover_big/nocover.png`
+          }
           alt="game cover"
           width={500}
           height={750}
@@ -16,6 +20,7 @@ const Game = ({ game }) => {
       <Link href="/" passHref>
         <button>{game.name}</button>
       </Link>
+      {JSON.stringify(game.cover)}
     </div>
   );
 };
