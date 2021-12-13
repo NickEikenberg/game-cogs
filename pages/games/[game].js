f0df9f13b4dada9a8a9bf989b7d200f5bb5f2867
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-13 02:42:21 -0500

    Search and games show pages work

--- pages/games/[game].js
+++ pages/games/[game].js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import Header from '../../src/components/Header/Header';
 
-const game = () => {
+const Game = () => {
+  const router = useRouter();
+  const { game } = router.query;
+  let [currentGame, setCurrentGame] = useState();
+
+  useEffect(() => {
+    const getGame = () => {
+      let data = `fields name, cover.image_id, summary,rating,storyline;\nwhere id = ${game};\n`;
+
+      let config = {
+        method: 'post',
+        url: 'https://peaceful-tor-54406.herokuapp.com/https://api.igdb.com/v4/games/',
+        headers: {
+          'Client-ID': 'dmlu2pmbd2wc14ke1ooh3on7cmnxr9',
+          Authorization: 'Bearer yrehr7e8zvm73i51nafp0w0c8d74ra',
+          'Content-Type': 'text/plain',
+        },
+        data: data,
+      };
+
+      axios(config)
+        .then((response) => {
+          setCurrentGame(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getGame();
+  }, [game]);
+
   return (
     <div>
-      <h1>Game show page</h1>
+      <Header />
+      {JSON.stringify(currentGame)}
+      {currentGame ? <h1>Show page for {currentGame[0].name}</h1> : null}
     </div>
   );
 };
 
-export default game;
+export default Game;
--- pages/search/Game.js
+++ pages/search/Game.js
@@ -5,12 +5,14 @@ import Link from 'next/link';
 const Game = ({ game }) => {
   return (
     <div className="m-2">
-      <Image
-        src="https://images.igdb.com/igdb/image/upload/t_cover_big/nocover.png"
-        alt="game cover"
-        width={500}
-        height={750}
-      ></Image>
+      <Link href={`/games/${game.id}`} passHref>
+        <Image
+          src="https://images.igdb.com/igdb/image/upload/t_cover_big/nocover.png"
+          alt="game cover"
+          width={500}
+          height={750}
+        ></Image>
+      </Link>
       <Link href="/" passHref>
         <button>{game.name}</button>
       </Link>
