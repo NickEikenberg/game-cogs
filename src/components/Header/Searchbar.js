18cfa0e187677485f2cc06384891249b8bae2e94
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-22 15:39:11 -0500

    User can hit enter on search bar to trigger their search

--- src/components/Header/Searchbar.js
+++ src/components/Header/Searchbar.js
@@ -4,11 +4,18 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const Searchbar = () => {
-  let [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchFocus, setSearchFocus] = useState(false);
 
   useEffect(() => {
     searchQuery.length > 0 ? console.log(searchQuery) : null;
   }, [searchQuery]);
+
+  const changeFocus = () => {
+    setSearchFocus(true);
+  };
+
+  const searchBtn = React.createRef();
 
   return (
     <div className="relative text-black">
@@ -17,9 +24,13 @@ const Searchbar = () => {
         name="search"
         placeholder="Search"
         onChange={(e) => setSearchQuery(e.target.value)}
+        onFocus={changeFocus}
+        onKeyPress={(e) =>
+          e.key === 'Enter' ? searchBtn.current.click() : null
+        }
       />
       <Link href={`/search/${searchQuery}`} passHref>
-        <button className="absolute right-1 top-0 mt-2 mr-2">
+        <button className="absolute right-1 top-0 mt-2 mr-2" ref={searchBtn}>
           <FaSearch />
         </button>
       </Link>
