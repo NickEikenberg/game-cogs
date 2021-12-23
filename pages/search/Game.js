9823c7becebff5f84c159858f169fc5944b0dc27
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-23 12:26:19 -0500

    heroku deploy

--- pages/search/Game.js
+++ pages/search/Game.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Game = ({ game }) => {
+  const [currentGame, setCurrentGame] = useState();
+
+  useEffect(() => {
+    game !== undefined ? setCurrentGame(game) : null;
+  }, [game]);
+
   return (
     <div className="m-2">
-      <Link href={`/games/${game.id}`} passHref>
+      <Link href={`/games/${currentGame.id}`} passHref>
         <Image
           src={
-            game.hasOwnProperty('cover')
-              ? `https://images.igdb.com/igdb/image/upload/t_1080p/${game.cover.image_id}.jpg`
+            currentGame.hasOwnProperty('cover')
+              ? `https://images.igdb.com/igdb/image/upload/t_1080p/${currentGame.cover.image_id}.jpg`
               : `https://images.igdb.com/igdb/image/upload/t_cover_big/nocover.png`
           }
           alt="game cover"
@@ -18,8 +24,12 @@ const Game = ({ game }) => {
           className="cursor-pointer"
         ></Image>
       </Link>
-      <Link href={`/games/${game.id}`} passHref className="cursor-pointer">
-        <button>{game.name}</button>
+      <Link
+        href={`/games/${currentGame.id}`}
+        passHref
+        className="cursor-pointer"
+      >
+        <button>{currentGame.name}</button>
       </Link>
     </div>
   );
